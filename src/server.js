a905cc198296a104780ce0749f29f96f2a94f4a7
U-Honolulu\Troy <me@bitcointroy.co>
2018-02-26 15:58:14 -0600

    user stuff begun

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const session = require('express-session');
+const bcrypt = require('bcrypt');
 
 const STATUS_USER_ERROR = 422;
 const BCRYPT_COST = 11;
@@ -8,27 +9,49 @@ const BCRYPT_COST = 11;
 const server = express();
 // to enable parsing of json bodies for post requests
 server.use(bodyParser.json());
-server.use(session({
-  secret: 'e5SPiqsEtjexkTj3Xqovsjzq8ovjfgVDFMfUzSmJO21dtXs4re'
-}));
+server.use(
+	session({
+		secret: 'e5SPiqsEtjexkTj3Xqovsjzq8ovjfgVDFMfUzSmJO21dtXs4re'
+	})
+);
 
 /* Sends the given err, a string or an object, to the client. Sets the status
  * code appropriately. */
 const sendUserError = (err, res) => {
-  res.status(STATUS_USER_ERROR);
-  if (err && err.message) {
-    res.json({ message: err.message, stack: err.stack });
-  } else {
-    res.json({ error: err });
-  }
+	res.status(STATUS_USER_ERROR);
+	if (err && err.message) {
+		res.json({ message: err.message, stack: err.stack });
+	} else {
+		res.json({ error: err });
+	}
 };
 
-// TODO: implement routes
-
+server.post('/users', (req, res) => {
+	const username = req.body.username;
+	const passwordHash = bcrypt.hash(
+		req.body.password,
+		BCRYPT_COST,
+		(err, hash) => {
+			if (err) {
+				throw err;
+			}
+			return hash;
+		}
+	);
+	const user = new User(username, passwordHash);
+	user
+		.save()
+		.then(newUser => {
+			res.status(201).json(newUser);
+		})
+		.catch(err => {
+			sendUserError(err, res);
+		});
+});
 // TODO: add local middleware to this route to ensure the user is logged in
 server.get('/me', (req, res) => {
-  // Do NOT modify this route handler in any way.
-  res.json(req.user);
+	// Do NOT modify this route handler in any way.
+	res.json(req.user);
 });
 
 module.exports = { server };
